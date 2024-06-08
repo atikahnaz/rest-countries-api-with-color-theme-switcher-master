bec5c44c89d424b50416490de27cba4fdd5cec07
Atikah <atikahnazir6@gmail.com>
2024-06-08 21:58:01 +0800

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import NavigationBar from "./components/Nav.jsx";
 import DisplayCountries from "./components/DisplayCountries.jsx";
-
+import ResultCountry from "./components/ResultCountry.jsx";
 import "./App.css";
 
 function App() {
+  async function ListCountries() {
+    const countries = await fetch("https://restcountries.com/v3.1/all").then(
+      (response) => response.json()
+    );
+    console.log(countries);
+  }
+
+  useEffect(() => {
+    ListCountries();
+  }, []);
+
   return (
     <>
       <NavigationBar></NavigationBar>
       <DisplayCountries />
+      <ResultCountry />
       <h1>hello</h1>
     </>
   );
---
+++ src/components/ResultCountry.jsx
@@ -0,0 +1,7 @@
+export default function ResultCountry() {
+  return (
+    <>
+      <button type="button">Back</button>
+    </>
+  );
+}
