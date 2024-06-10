8e1c244ccb7cf023258c1284a144718fe3f08599
Atikah <atikahnazir6@gmail.com>
2024-06-10 23:38:49 +0800

    loop object

--- src/components/ResultCountry.jsx
+++ src/components/ResultCountry.jsx
@@ -8,21 +8,15 @@ export default function ResultCountry({ selectedCountry }) {
 
   return (
     <>
-      <div className=" h-dvh text-FEWhite">
+      <div className=" h-dvh text-FEWhite ">
         <div>Back</div>
-        <div>
+        <div className=" ">
           <img src={selectedCountry[0].flags.png} alt="" />
-          <h3>{selectedCountry[0].name.official}</h3>
-
-          <p>Native Name: {selectedCountry[0].name.nativeName.eng.official}</p>
-          <p>Region: {selectedCountry[0].region}</p>
-          <p>Sub Region: {selectedCountry[0].subregion}</p>
-          <p>Capital: {selectedCountry[0].capital}</p>
-
-          {/** 
           <div>
             <h3>{selectedCountry[0].name.official}</h3>
-            <p>Native Name: {selectedCountry[0].name.nativeName}</p>
+            <p>
+              Native Name: {selectedCountry[0].name.nativeName.eng.official}
+            </p>
             <p>Population: {selectedCountry[0].population}</p>
             <p>Region: {selectedCountry[0].region}</p>
             <p>Sub Region: {selectedCountry[0].subregion}</p>
@@ -32,9 +26,18 @@ export default function ResultCountry({ selectedCountry }) {
           <div>
             <p>Top Level Domain: </p>
             <p>Currencies: {selectedCountry.currencies}</p>
-            <p>Languages: </p>
+            <p>
+              Languages:
+              {Object.entries(selectedCountry[0].languages).map(
+                ([key, value], index, array) => (
+                  <span>
+                    {value}
+                    {index < array.length - 1 && <span>, </span>}
+                  </span>
+                )
+              )}
+            </p>
           </div>
-*/}
         </div>
       </div>
     </>
