99a554c973f5517eeffaefae45c7829e854be6e6
Atikah <atikahnazir6@gmail.com>
2024-06-10 18:38:10 +0800

    render data

--- src/components/DisplayCountries.jsx
+++ src/components/DisplayCountries.jsx
@@ -14,7 +14,11 @@ export default function DisplayCountries({ countries }) {
       <div className=" flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {countries.map((country, index) => (
           <div className="mb-6 max-w-72  bg-FEDarkBlue text-FEWhite m-auto">
-            <img className="m-auto" src={country.flags.png} alt="" />
+            <img
+              className="m-auto w-72 h-auto"
+              src={country.flags.png}
+              alt=""
+            />
 
             <div className="px-4 py-7 ">
               <h3 className=" font-semibold">{country.name.official}</h3>
--- src/components/ResultCountry.jsx
+++ src/components/ResultCountry.jsx
@@ -1,7 +1,42 @@
-export default function ResultCountry() {
+export default function ResultCountry({ selectedCountry }) {
+  console.log(selectedCountry);
+  console.log(selectedCountry[0].name.official);
+  console.log(selectedCountry[0].currencies);
+  console.log(selectedCountry[0].languages);
+
+  // if return object use object.entries()
+
   return (
     <>
-      <div>back</div>
+      <div className=" h-dvh text-FEWhite">
+        <div>Back</div>
+        <div>
+          <img src={selectedCountry[0].flags.png} alt="" />
+          <h3>{selectedCountry[0].name.official}</h3>
+
+          <p>Native Name: {selectedCountry[0].name.nativeName.eng.official}</p>
+          <p>Region: {selectedCountry[0].region}</p>
+          <p>Sub Region: {selectedCountry[0].subregion}</p>
+          <p>Capital: {selectedCountry[0].capital}</p>
+
+          {/** 
+          <div>
+            <h3>{selectedCountry[0].name.official}</h3>
+            <p>Native Name: {selectedCountry[0].name.nativeName}</p>
+            <p>Population: {selectedCountry[0].population}</p>
+            <p>Region: {selectedCountry[0].region}</p>
+            <p>Sub Region: {selectedCountry[0].subregion}</p>
+            <p>Capital: {selectedCountry[0].capital}</p>
+          </div>
+
+          <div>
+            <p>Top Level Domain: </p>
+            <p>Currencies: {selectedCountry.currencies}</p>
+            <p>Languages: </p>
+          </div>
+*/}
+        </div>
+      </div>
     </>
   );
 }
