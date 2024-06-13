81dd838b8203bba1bbf1ceb0ad8e7583a4751252
Atikah <atikahnazir6@gmail.com>
2024-06-13 18:37:08 +0800

    toggle navbar color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,33 @@
-export default function NavigationBar() {
+import { useState, useEffect } from "react";
+import Moon from "../images/light-mode.png";
+import "../index.css";
+
+export default function NavigationBar({ changeMode }) {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+  }
+  useEffect(() => {
+    console.log(darkMode);
+    changeMode(darkMode);
+  }, [darkMode]);
+
   return (
     <>
-      <div className="flex justify-between bg-FEDarkBlue text-FEWhite px-5 py-8">
+      <div
+        className={`flex justify-between ${
+          darkMode ? "dark-mode" : "light-mode"
+        }  text-FEWhite px-5 py-8`}
+      >
         <div>Where in the world?</div>
-        <div>Dark Mode</div>
+
+        <div className="flex items-center h-5" onClick={toggleDarkMode}>
+          <div className="h-full">
+            <img src={Moon} alt="Moon" className="h-full" />
+          </div>
+          <div className="ml-2">Dark Mode</div>
+        </div>
       </div>
     </>
   );
